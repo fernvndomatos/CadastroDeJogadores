ae422e79ee1a9c280322f4f83b78d3e377a84fc1
Fernando Matos <fernando.matos221@gmail.com>
2026-07-31 16:06:33 -0300

    feat: Cria JogadorRepository e ClubeRepository

---
+++ src/main/java/com/github/fernvndomatos/CadastroDeJogadores/Clubes/ClubeRepository.java
@@ -0,0 +1,6 @@
+package com.github.fernvndomatos.CadastroDeJogadores.Clubes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubeRepository extends JpaRepository<ClubeModel, Long> {
+}
---
+++ src/main/java/com/github/fernvndomatos/CadastroDeJogadores/Jogadores/JogadorRepository.java
@@ -0,0 +1,6 @@
+package com.github.fernvndomatos.CadastroDeJogadores.Jogadores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JogadorRepository extends JpaRepository<JogadorModel, Long> {
+}
